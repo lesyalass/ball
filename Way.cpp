f8bb0ec9bd1a1cd8d1f78fc986605f02fa14ad7d
lesyalass <71562447+lesyalass@users.noreply.github.com>
2020-09-26 13:45:49 +0300

    new Structure Vector

--- Way.cpp
+++ Way.cpp
@@ -2,13 +2,16 @@
 #include <cstdlib>
 #include <cmath>
 
-
-struct Sphere
+struct Vector
 {
     float X;
     float Y;
-    float velocityX;
-    float velocityY;
+};
+
+struct Sphere
+{
+    Vector position;
+    Vector velocity;
     int Radius;
     int redRate;
     int greenRate;
@@ -20,24 +23,24 @@ void drawSphere(Sphere* sphere)
 {
     for (int i = 0; i < sphere->Gradient; i++)
     {
-        int X = sphere->X + sphere->Radius * i / 3 / sphere->Gradient;
-        int Y = sphere->Y + sphere->Radius * i / 2 / sphere->Gradient;
+        int X = sphere->position.X + sphere->Radius * i / 3 / sphere->Gradient;
+        int Y = sphere->position.Y + sphere->Radius * i / 2 / sphere->Gradient;
 
         txSetFillColor(RGB(i * sphere->redRate / sphere->Gradient, i * sphere->greenRate / sphere->Gradient, i * sphere->blueRate / sphere->Gradient));
         txSetColor    (RGB(i * sphere->redRate / sphere->Gradient, i * sphere->greenRate / sphere->Gradient, i * sphere->blueRate / sphere->Gradient));
-        txCircle(sphere->X, sphere->Y, sphere->Radius - sphere->Radius * i / sphere->Gradient);
+        txCircle(sphere->position.X, sphere->position.Y, sphere->Radius - sphere->Radius * i / sphere->Gradient);
     }
 }
 
 void moveSphere(Sphere *sphere, float dt)
 {
-    sphere->X += sphere->velocityX * dt;
-    sphere->Y += sphere->velocityY * dt;
+    sphere->position.X += sphere->velocity.X * dt;
+    sphere->position.Y += sphere->velocity.Y * dt;
 }
 
 bool checkCollisionTwoSpheres(Sphere* sphere1, Sphere* sphere2)
 {
-    if (sphere1->Radius + sphere2->Radius > sqrt(pow(sphere1->X - sphere2->X, 2)+ pow(sphere1->Y - sphere2->Y, 2)))
+    if (sphere1->Radius + sphere2->Radius > sqrt(pow(sphere1->position.X - sphere2->position.X, 2)+ pow(sphere1->position.Y - sphere2->position.Y, 2)))
     {
         return true;
     }
@@ -46,14 +49,14 @@ bool checkCollisionTwoSpheres(Sphere* sphere1, Sphere* sphere2)
 
 void collisionSphere(Sphere* sphere, int screenSizeX, int screenSizeY)
 {
-    if (sphere->X > screenSizeX - sphere->Radius || sphere->X < sphere->Radius)
+    if (sphere->position.X > screenSizeX - sphere->Radius || sphere->position.X < sphere->Radius)
         {
-            sphere->velocityX = - sphere->velocityX;
+            sphere->velocity.X = - sphere->velocity.X;
         }
 
-    if (sphere->Y > screenSizeY - sphere->Radius || sphere->Y < sphere->Radius)
+    if (sphere->position.Y > screenSizeY - sphere->Radius || sphere->position.Y < sphere->Radius)
         {
-            sphere->velocityY = - sphere->velocityY;
+            sphere->velocity.Y = - sphere->velocity.Y;
         }
 }
 
@@ -68,19 +71,19 @@ float projectionVector( float vectorX, float vectorY, float axisX, float axisY)
 
 void changeVelocitySphere(Sphere* sphere1, Sphere* sphere2)
 {
-    float axisX = sphere1->X - sphere2->X;
-    float axisY = sphere1->Y - sphere2->Y;
+    float axisX = sphere1->position.X - sphere2->position.X;
+    float axisY = sphere1->position.Y - sphere2->position.Y;
     float moduleAxis = pow (axisX * axisX + axisY * axisY, 0.5 );
-    float projectionVelocity1 = projectionVector( sphere1->velocityX, sphere1->velocityY, axisX, axisY);
-    float projectionVelocity2 = projectionVector( sphere2->velocityX, sphere2->velocityY, axisX, axisY);
+    float projectionVelocity1 = projectionVector( sphere1->velocity.X, sphere1->velocity.Y, axisX, axisY);
+    float projectionVelocity2 = projectionVector( sphere2->velocity.X, sphere2->velocity.Y, axisX, axisY);
     float dv = projectionVelocity1 - projectionVelocity2;
 
     if (dv < 0)
     {
-        sphere2->velocityX +=   dv * axisX / moduleAxis;
-        sphere2->velocityY +=   dv * axisY / moduleAxis;
-        sphere1->velocityX += - dv * axisX / moduleAxis;
-        sphere1->velocityY += - dv * axisY / moduleAxis;
+        sphere2->velocity.X +=   dv * axisX / moduleAxis;
+        sphere2->velocity.Y +=   dv * axisY / moduleAxis;
+        sphere1->velocity.X += - dv * axisX / moduleAxis;
+        sphere1->velocity.Y += - dv * axisY / moduleAxis;
     }
 }
 
@@ -91,9 +94,9 @@ int main()
 
 
     float dt = 1;
-    Sphere sphere1 = {100, 200, 2.0, 2.0, 20, 300, 0, 300, 300};
-    Sphere sphere2 = {200, 200, 2.0, 2.0, 20, 0, 300, 300, 300};
-    Sphere sphere3 = {300, 200, 2.0, 2.0, 20, 300, 300, 0, 300};
+    Sphere sphere1 = {Vector{100, 200}, Vector{2.0, 2.0}, 20, 300, 0, 300, 300};
+    Sphere sphere2 = {Vector{200, 200}, Vector{2.0, 2.0}, 20, 0, 300, 300, 300};
+    Sphere sphere3 = {Vector{300, 200}, Vector{2.0, 2.0}, 20, 300, 300, 0, 300};
 
     txCreateWindow(screenSizeX, screenSizeY);
     txSetColor(RGB(0, 0, 0));
@@ -112,10 +115,10 @@ int main()
 
         if (txMouseButtons() > 0 )
         {
-            sphere1.X = mouseX;
-            sphere1.Y = mouseY;
-            sphere1.velocityX = (mouseX - mouseOldX)/dt;
-            sphere1.velocityY = (mouseY - mouseOldY)/dt;
+            sphere1.position.X = mouseX;
+            sphere1.position.Y = mouseY;
+            sphere1.velocity.X = (mouseX - mouseOldX)/dt;
+            sphere1.velocity.Y = (mouseY - mouseOldY)/dt;
         }
 
         moveSphere(&sphere1, dt);
